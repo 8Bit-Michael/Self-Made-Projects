3119e9e524373c262125dda3280b3e722c5910e5
MichaelJS <mowz2012@gmail.com>
2025-06-30 08:17:34 -0700

    Add files via upload

--- Grocery_Store_System.py
+++ Grocery_Store_System.py
@@ -13,7 +13,7 @@ class Inventory:
 
     def list_products(self):
         for product in self.products.values():
-            print(Fore.GREEN + f"Product: {product.name} Price: ${product.price} Stock: {product.stock}")
+            print(Fore.GREEN + f"| Product: {product.name} | Price: ${product.price:.2f} | Stock: {product.stock} |")
 
     def add_product(self, product):
         self.products[product.name] = product
@@ -29,7 +29,7 @@ class Product:
         self.stock = stock
 
     def __str__(self):
-        return f"Product: {self.name} Price: ${self.price} Stock: {self.stock}"
+        return f"Product: {self.name} Price: ${self.price:.2f} Stock: {self.stock}"
 
     def change_stock(self, amount):
         self.stock += amount
@@ -55,9 +55,8 @@ class Cart:
 
     def view_cart(self):
         if not self.items:
-            return "Cart is empty."
-        word_count = len(self.items)
-        return Fore.GREEN + f"The {word_count} items in your cart include the following:\n" + "\n".join(str(item) for item in self.items)
+            return "Your cart is empty."
+        return f"The {len(self.items)} items in your cart include the following:\n" + "\n".join(str(item) for item in self.items)
 
     def calculate_total(self):
         total = sum(p.price for p in self.items)
@@ -75,89 +74,120 @@ class Store:
 
     def start(self):
         while True:
-            user_role = input("Welcome to Tom's grocery store, are you an admin or a customer? (type 'exit' to quit) ").lower()
-            if user_role == 'exit':
-                print(Fore.YELLOW + "Thanks for visiting Tom's grocery store!")
+            try:
+                user_role = input("Welcome to Tom's grocery store, are you an admin or a customer? Type 'admin', 'customer', or 'exit' to quit: ").lower()
+                role_match = match_input(user_role, ['admin', 'customer', 'exit'])
+                if role_match and role_match[0] == 'admin':
+                    self.admin_actions()
+                elif role_match and role_match[0] == 'customer':
+                    self.customer_actions()
+                elif role_match and role_match[0] == 'exit':
+                    print("Thank you for visiting Tom's grocery store, goodbye!")
+                    break
+                else:
+                    print(Fore.RED + f"Sorry, the role '{user_role}' wasn't recognized.")
+            except (KeyboardInterrupt, EOFError):
+                print("\n" + Fore.YELLOW + "Session terminated. Goodbye!")
                 break
 
-            role_match = match_input(user_role, ['admin', 'customer'])
-            if role_match:
-                role = role_match[0]
-                if role == 'admin':
-                    self.admin_actions()
-                elif role == 'customer':
-                    self.customer_actions()
-            else:
-                print(Fore.RED + f"Sorry, the role '{user_role}' wasn't recognized.")
-
     def admin_actions(self):
-        admin_name = input("What is your name: ")
-        name = admin_name.capitalize()
-        admin_choice = input(f"""What would you like to do as an admin, {name}? 
-Please type 'quantity' to set product amounts, 'product name' to rename items, or 'inventory' to change stock levels:\n""")
-        action = match_input(admin_choice, ['quantity', 'product name', 'inventory'])
+        while True:
+            admin_name = input("What is your name: ")
+            if admin_name.isalpha():
+                name = admin_name.capitalize()
+                break
+            else:
+                print(Fore.RED + f"The name '{admin_name}' is invalid, please try again.")
 
-        if action and action[0] == 'quantity':
-            product_input = input("Which product's quantity would you like to change: ")
-            product_match = match_input(product_input, list(self.inventory.products.keys()))
-            if product_match:
-                product = self.inventory.products[product_match[0]]
-                user_amount = input("How much would you like to add or remove? Type a number, like 8 or -19: ")
-                try:
-                    amount = int(user_amount)
-                    product.change_stock(amount)
-                    print(Fore.GREEN + f"Your update was successful! You now have {product.stock} of '{product.name}' in stock.")
-                except ValueError:
-                    print(Fore.RED + f"{user_amount} is not a valid number. You must type in a number, like 12.")
-            else:
-                print(Fore.RED + f"'{product_input}' isn't a valid option.")
-        elif action and action[0] == 'product name':
-            user_choice = input("What product do you want to rename? ")
-            product_match = match_input(user_choice, list(self.inventory.products.keys()))
-            if product_match:
-                product = self.inventory.products[product_match[0]]
-                new_name = input(f"What would you like to rename '{product.name}' to? ")
-                if new_name.isdigit():
-                    grammatic_new_name = string.capwords(new_name)
-                    print(Fore.RED + f"Your new name for the product cannot be a digit. '{grammatic_new_name}' is invalid.")
-                elif new_name in self.inventory.products:
-                    print(Fore.RED + f"A product named '{new_name}' already exists.")
+        while True:
+            admin_choice = input(f"""What would you like to do as an admin, {name}? 
+Please type 'quantity' to set product amounts, 'product name' to rename items, or 'inventory' to check stock levels: """)
+            action = match_input(admin_choice, ['quantity', 'product name', 'inventory'])
+
+            if action and action[0] == 'quantity':
+                product_input = input("Which product's quantity would you like to change: ")
+                product_match = match_input(product_input, list(self.inventory.products.keys()))
+                if product_match:
+                    product = self.inventory.products[product_match[0]]
+                    user_amount = input("How much would you like to add or remove? Type a number, like 8 or -19: ")
+                    try:
+                        amount = int(user_amount)
+                        if product.stock + amount < 0:
+                            print(Fore.RED + "The stock cannot be negative, please try again.")
+                        else:
+                            product.change_stock(amount)
+                            print(Fore.GREEN + f"Your update was successful! You now have {product.stock} of '{product.name}' in stock.")
+                    except ValueError:
+                        print(Fore.RED + f"{user_amount} is not a valid number. You must type in a number, like 12.")
                 else:
-                    self.inventory.products.pop(product.name)
-                    product.name = string.capwords(new_name)
-                    self.inventory.products[product.name] = product
-                    print(Fore.GREEN + f"You have now rebranded the product to '{product.name}'!")
+                    print(Fore.RED + f"'{product_input}' isn't a valid option.")
+
+            elif action and action[0] == 'product name':
+                user_choice = input("What product do you want to rename? ")
+                product_match = match_input(user_choice, list(self.inventory.products.keys()))
+                if product_match:
+                    product = self.inventory.products[product_match[0]]
+                    new_name = input(f"What would you like to rename '{product.name}' to? ").strip()
+                    new_name_clean = string.capwords(new_name)
+                    if not new_name_clean.replace(" ", "").isalpha():
+                        print(Fore.RED + f"'{new_name_clean}' is invalid. Product names cannot contain digits or symbols.")
+                    elif new_name_clean in self.inventory.products:
+                        print(Fore.RED + f"A product named '{new_name_clean}' already exists.")
+                    else:
+                        self.inventory.products.pop(product.name)
+                        product.name = new_name_clean
+                        self.inventory.products[product.name] = product
+                        print(Fore.GREEN + f"You have now rebranded the product to '{product.name}'!")
+                else:
+                    print(Fore.RED + f"The product '{user_choice}' doesn't seem to be in the grocery store, please try again.")
+
+            elif action and action[0] == 'inventory':
+                self.inventory.list_products()
             else:
-                print(Fore.RED + f"The product '{user_choice}' doesn't seem to be in the grocery store, please try again.")
-        elif action and action[0] == 'inventory':
-            self.inventory.list_products()
-        else:
-            print(Fore.RED + f"'{admin_choice}' is an invalid choice.")
+                print(Fore.RED + f"'{admin_choice}' is an invalid choice.")
 
     def customer_actions(self):
-        customer_name = input("What is your name: ")
-        name = customer_name.capitalize()
+        while True:
+            customer_name = input("What is your name: ")
+            if customer_name.isalpha():
+                name = customer_name.capitalize()
+                break
+            else:
+                print(Fore.RED + f"The name '{customer_name}' is not valid. Please try again.")
+
         user = User(name)
         self.users.append(user)
 
-        customer_choice = input(f"Would you like to add an item to your cart, {name}? Type yes or no: ")
-        refined_customer_choice = match_input(customer_choice, ['yes', 'no'])
+        while True:
+            customer_choice = input(f"Would you like to add an item to your cart, {name}? Type 'yes', 'no', 'checkout', or 'exit': ")
+            refined_customer_choice = match_input(customer_choice, ['yes', 'no', 'checkout', 'exit'])
 
-        if refined_customer_choice and refined_customer_choice[0] == 'yes':
-            item_addition = input("What item would you like to add: ")
-            refined_item_addition = match_input(item_addition, list(self.inventory.products.keys()))
-            if refined_item_addition:
-                product = self.inventory.products[refined_item_addition[0]]
-                user.cart.add_to_cart(product)
-                print(Fore.GREEN + f"{refined_item_addition[0]} has been added to your cart!")
+            if refined_customer_choice and refined_customer_choice[0] == 'yes':
+                item_addition = input("What item would you like to add: ")
+                refined_item_addition = match_input(item_addition, list(self.inventory.products.keys()))
+                if refined_item_addition:
+                    product = self.inventory.products[refined_item_addition[0]]
+                    user.cart.add_to_cart(product)
+                    print(Fore.GREEN + f"{refined_item_addition[0]} has been added to your cart!")
+                else:
+                    print(Fore.RED + f"'{item_addition.capitalize()}' is not a valid option.")
+
+            elif refined_customer_choice and refined_customer_choice[0] == 'checkout':
+                print(Fore.GREEN + "Thank you for shopping with us!")
+                print(user.cart.view_cart())
+                print(user.cart.calculate_total())
+                return
+
+            elif refined_customer_choice and refined_customer_choice[0] == 'exit':
+                print("Thank you for visiting Tom's grocery store!")
+                return
+
+            elif refined_customer_choice and refined_customer_choice[0] == 'no':
+                print(Fore.GREEN + "Okay! Let us know if you need anything.")
+                return
+
             else:
-                punctuated_item_addition = item_addition.capitalize()
-                print(Fore.RED + f"'{punctuated_item_addition}' is not a valid option.")
-                print(Fore.YELLOW + "Please try again.\n")
-        elif refined_customer_choice and refined_customer_choice[0] == 'no':
-            print(Fore.GREEN + "Okay! Let us know if you need anything.")
-        else:
-            print(Fore.RED + f"'{customer_choice}' is an invalid choice.")
+                print(Fore.RED + f"'{customer_choice}' is an invalid input. Please try again.")
 
 if __name__ == '__main__':
     inventory = Inventory()
